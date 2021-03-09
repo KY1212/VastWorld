49125a8f64040b946ff49e2d4aa27f27c8f0bfab
KY1212 <YumYum1212@docomo.ne.jp>
2021-03-10 06:11:17 +0900

    formの

---
+++ contact-form.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Template Name: contact-form
+*/
+?>
+
+<?php get_header(); ?>
+<main class="l-main">
+	<div class="p-container__inner">
+		<h3 class="headingTop">
+			お問い合わせ
+		</h3>
+	<?php the_content(); ?>
+</div>
+</main>
+	<?php get_footer(); ?>
